dec59edbe051672289c3a142bc52715962b6d65f
Christopher Fournier <chris.d.fournier@gmail.com>
2019-01-25 08:36:11 -0500

    New 'screens' directory moved to 'ux' 'app' directory
    Also created 'flows" directory

--- ux/assets/scripts/mystuff.header.js
+++
@@ -1,8 +0,0 @@
-// HEADROOM
-var myElement = document.querySelector("header");
-var headroom  = new Headroom(myElement);
-headroom.init(); 
-
-// MAIN PADDING
-var headerHeight = header.offsetHeight;
-$("main").css("padding-top", headerHeight);
